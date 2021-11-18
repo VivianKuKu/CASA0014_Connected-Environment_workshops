3296fd7992312c92f5f079ec58167b2b8bf45248
Vivian Ku <52306317+VivianKuKu@users.noreply.github.com>
2021-11-18 00:09:14 +0000

    Update testMQTT_updated.ino

--- Plant Monitor/testMQTT_updated/testMQTT_updated.ino
+++ Plant Monitor/testMQTT_updated/testMQTT_updated.ino
@@ -12,10 +12,10 @@
 #define SECRET_MQTTPASS "password";
  */
 
-const char* ssid     = "CE-Hub";
-const char* password = "()c454,w1f1";
-const char* mqttuser = "student";
-const char* mqttpass = "ce2021-mqtt-forget-whale";
+const char* ssid     = "ssid name";
+const char* password = "ssid password";
+const char* mqttuser = "user name - eg student";
+const char* mqttpass = "password";
 
 const char* mqtt_server = "mqtt.cetools.org";
 
